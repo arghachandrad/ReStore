0aace3928767f8dbcbc126c65f55abb0e5d65570
arghachdas <arghachandra@technoexponent.com>
2022-01-12 20:37:52 +0530

    Removed cors error

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/api/products")
+            .then((res) => res.json())
+            .then((data) => setProducts(data));
+    }, []);
+
+    return (
+        <div>
+            <h1>Re-Store</h1>
+            <p>{products.length}</p>
+            {/* <ul>
+              {
+                products.map((product, index) => (
+                  <li key={index}>{product.}</li>
+                ))
+              }
+            </ul> */}
+        </div>
+    );
 }
 
 export default App;
