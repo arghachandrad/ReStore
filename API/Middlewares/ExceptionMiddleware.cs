4687a20cde204a804dafbac02923403cbd905e78
arghachdas <arghachandra@technoexponent.com>
2022-01-13 21:47:06 +0530

    Created own exception handler middleware in order to send proper formatted error response

---
+++ API/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace API.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        private readonly IHostEnvironment _env;
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+        {
+            _env = env;
+            _logger = logger;
+            _next = next;
+
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                // Here we are handling the exception by ourselves
+                _logger.LogError(ex, ex.Message);
+                // generating a response that we can sent back to client
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = 500; // internal server error
+
+                var response = new ProblemDetails
+                {
+                    Status = 500,
+                    Detail = _env.IsDevelopment() ? ex.StackTrace?.ToString() : null,
+                    Title = ex.Message
+                };
+
+                var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+                // send json response to client in CamelCase
+                var json = JsonSerializer.Serialize(response, options);
+
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+}
