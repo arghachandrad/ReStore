56854088db55a7239a8599634ed9684c68b557f2
arghachdas <arghachandra@technoexponent.com>
2022-01-14 12:24:42 +0530

    Accessing history object outside any react component and sending some useful data along with it and using Router instead of BrowserRouter

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
 import { toast } from "react-toastify";
+import { history } from "../..";
 
 axios.defaults.baseURL = "http://localhost:5000/api/";
 
@@ -19,13 +20,26 @@ axios.interceptors.response.use(
     switch (status) {
       // 404 we will handle inside component
       case 400:
+        if (data.errors) {
+          // means if validation errors
+          const modelStateErrors: string[] = [];
+          for (const key in data.errors) {
+            modelStateErrors.push(data.errors[key]);
+          }
+          throw modelStateErrors.flat();
+        }
         toast.error(data.title);
         break;
       case 401:
         toast.error(data.title);
         break;
       case 500:
-        toast.error(data.title);
+        // also need to send the server error to ServerError Component
+        history.push({
+          pathname: "/server-error",
+          state: { error: data },
+        });
+        // toast.error(data.title);
         break;
       default:
         break;
--- client/src/features/about/AboutPage.tsx
+++ client/src/features/about/AboutPage.tsx
@@ -1,7 +1,30 @@
-import { Button, ButtonGroup, Container, Typography } from "@mui/material";
+import {
+  Alert,
+  AlertTitle,
+  Button,
+  ButtonGroup,
+  Container,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
 import agent from "../../app/api/agent";
 
 const AboutPage = () => {
+  const [validationErrors, setValidationErrors] = useState<string[]>([]);
+
+  const getValidationErrors = async () => {
+    try {
+      const res = await agent.TestErrors.getValidationError();
+      console.log("should not see this:--", res);
+    } catch (error) {
+      const err = error as string[];
+      console.log(err);
+      setValidationErrors(err);
+    }
+  };
   return (
     <Container>
       <Typography gutterBottom variant="h2">
@@ -29,13 +52,22 @@ const AboutPage = () => {
         <Button variant="contained" onClick={async () => await agent.TestErrors.get500Error()}>
           Test 500 Error
         </Button>
-        <Button
-          variant="contained"
-          onClick={async () => await agent.TestErrors.getValidationError()}
-        >
+        <Button variant="contained" onClick={getValidationErrors}>
           Test Validation Error
         </Button>
       </ButtonGroup>
+      {validationErrors.length > 0 && (
+        <Alert severity="error">
+          <AlertTitle>ValidationErrors</AlertTitle>
+          <List>
+            {validationErrors.map((error) => (
+              <ListItem key={error}>
+                <ListItemText>{error}</ListItemText>
+              </ListItem>
+            ))}
+          </List>
+        </Alert>
+      )}
     </Container>
   );
 };
