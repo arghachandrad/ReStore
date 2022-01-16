87bb177ca33bf2ea9a401c11ca754e9a0c18372b
arghachdas <arghachandra@technoexponent.com>
2022-01-16 21:26:53 +0530

    Reduce and increase item count in basket and completely delete an item from basket is implement in BasketPage

--- client/src/features/basket/BasketPage.tsx
+++ client/src/features/basket/BasketPage.tsx
@@ -1,5 +1,7 @@
-import { Delete } from "@mui/icons-material";
+import { Add, Delete, Remove } from "@mui/icons-material";
+import { LoadingButton } from "@mui/lab";
 import {
+  Box,
   IconButton,
   Paper,
   Table,
@@ -10,10 +12,37 @@ import {
   TableRow,
   Typography,
 } from "@mui/material";
+import { useState } from "react";
+import agent from "../../app/api/agent";
 import { useStoreContext } from "../../app/context/StoreContext";
 
 const BasketPage = () => {
-  const { basket } = useStoreContext();
+  const { basket, setBasket, removeItem } = useStoreContext();
+  const [loading, setLoading] = useState(false);
+
+  const handleAddItem = async (productId: number) => {
+    setLoading(true);
+    try {
+      const basket = await agent.Basket.addItem(productId);
+      setBasket(basket);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  const handleRemoveItem = async (productId: number, quantity: number = 1) => {
+    setLoading(true);
+    try {
+      await agent.Basket.removeItem(productId, quantity);
+      removeItem(productId, quantity);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
 
   if (!basket) return <Typography variant="h3">Your basket is empty</Typography>;
 
@@ -24,7 +53,7 @@ const BasketPage = () => {
           <TableRow>
             <TableCell>Product</TableCell>
             <TableCell align="right">Price</TableCell>
-            <TableCell align="right">Quantity</TableCell>
+            <TableCell align="center">Quantity</TableCell>
             <TableCell align="right">Subtotal</TableCell>
             <TableCell align="right"></TableCell>
           </TableRow>
@@ -36,17 +65,46 @@ const BasketPage = () => {
               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {basketItem.name}
+                {
+                  <Box display="flex" alignItems="center">
+                    <img
+                      src={basketItem.pictureUrl}
+                      alt={basketItem.name}
+                      style={{ height: 50, marginRight: 20 }}
+                    />
+                    <span>{basketItem.name}</span>
+                  </Box>
+                }
               </TableCell>
               <TableCell align="right">${(basketItem.price / 100).toFixed(2)}</TableCell>
-              <TableCell align="right">{basketItem.quantity}</TableCell>
+              <TableCell align="center">
+                <LoadingButton
+                  loading={loading}
+                  color="error"
+                  onClick={() => handleRemoveItem(basketItem.productId)}
+                >
+                  <Remove />
+                </LoadingButton>
+                {basketItem.quantity}
+                <LoadingButton
+                  color="success"
+                  loading={loading}
+                  onClick={() => handleAddItem(basketItem.productId)}
+                >
+                  <Add />
+                </LoadingButton>
+              </TableCell>
               <TableCell align="right">
                 ${((basketItem.price * basketItem.quantity) / 100).toFixed(2)}
               </TableCell>
               <TableCell align="right">
-                <IconButton color="error">
+                <LoadingButton
+                  color="error"
+                  loading={loading}
+                  onClick={() => handleRemoveItem(basketItem.productId, basketItem.quantity)}
+                >
                   <Delete />
-                </IconButton>
+                </LoadingButton>
               </TableCell>
             </TableRow>
           ))}
